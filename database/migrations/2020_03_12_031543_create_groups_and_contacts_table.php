124825d4fdaa0517e9799c59504bff5148a8bee2
alhayat-dev <alhayat.dev@gmail.com>
2020-03-11 23:26:15 -0400

    Database connection & migrations table were made

---
+++ database/migrations/2020_03_12_031543_create_groups_and_contacts_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsAndContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('group_id')->default(0);
+            $table->string('name');
+            $table->string('company');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamps();
+
+            $table->foreign('group_id')
+                ->references('id')->on('groups')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('groups');
+
+    }
+}
